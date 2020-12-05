942deae6e6d54043680e31518f6f8986a073c6c4
MertYldrmm <mert.yyldrim@gmail.com>
2020-12-05 21:04:34 +0300

    readme file edit

---
+++ helper/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb+srv://movie-user:1907@movie-api.8iu0n.mongodb.net/movie-user?retryWrites=true&w=majority',{ useNewUrlParser: 
+    true, useUnifiedTopology: true });
+    
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB: Connected');
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+
+    mongoose.Promise = global.Promise;
+}
