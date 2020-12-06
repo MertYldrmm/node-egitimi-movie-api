37f8b368a8b405d209c17f768bdb6d5992a94491
MertYldrmm <mert.yyldrim@gmail.com>
2020-12-06 23:46:11 +0300

    /GET/:movie_id movie tests

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -5,7 +5,7 @@ const server = require('../../app');
 
 chai.use(chaiHttp);
 
-let token;
+let token, movieId;
 
 describe('/api/movies tests', () => {
     before((done) => {
@@ -55,6 +55,27 @@ describe('/api/movies tests', () => {
                     res.body.should.have.property('country');
                     res.body.should.have.property('year');
                     res.body.should.have.property('imdb_score');
+                    movieId = res.body._id;
+                    done();
+                });
+        });
+    });
+
+    describe('/GET/:movie_id movie', () => {
+        it('it should GET a movie by the given id', (done) => {
+            chai.request(server)
+                .get('/api/movies/'+ movieId)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movieId);
                     done();
                 });
         });
